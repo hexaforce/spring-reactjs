9c4d2965584c860c468abb67e1423bd209ef7152
relics9 <relics9@192.168.0.21>
2019-02-03 09:25:54 +0900

    aa

--- spring-reactjs-social/src/main/java/io/hexaforce/social/config/WebMvcConfig.java
+++ spring-reactjs-social/src/main/java/io/hexaforce/social/config/WebMvcConfig.java
@@ -1,9 +1,19 @@
 package io.hexaforce.social.config;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -18,5 +28,26 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .allowCredentials(true)
         .maxAge(MAX_AGE_SECS);
     }
+    
+    public class ApplicationHandlerInterceptor implements HandlerInterceptor {
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            log.info(request.getRequestURI());
+            return true;
+        }
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+            //log.info(request.getRequestURI());
+        }
+        @Override
+        public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+            //log.info(request.getRequestURI());
+        }
+    }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ApplicationHandlerInterceptor()).addPathPatterns("/**");
+    }
+    
 }
