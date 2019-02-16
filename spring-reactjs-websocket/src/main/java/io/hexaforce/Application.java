e302e2cc0a104ecba49773ab75b46cf267e6e7d1
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-02-16 16:37:48 +0900

    dd

--- spring-reactjs-websocket/src/main/java/io/hexaforce/Application.java
+++ spring-reactjs-websocket/src/main/java/io/hexaforce/Application.java
@@ -1,13 +1,13 @@
-package io.hexaforce.websocket;
+package io.hexaforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HexaforceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HexaforceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
